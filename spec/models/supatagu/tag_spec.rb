25452d77a539eb18c1f8ec0205697dd9cb049667
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-04-20 13:58:14 -0700

    Failed: Tag name validation and error message

--- spec/models/supatagu/tag_spec.rb
+++ spec/models/supatagu/tag_spec.rb
@@ -2,8 +2,20 @@ require 'spec_helper'
 
 module Supatagu
   describe Tag do
+    before(:each) do
+      @tag = Tag.create
+    end
+      
     it :exists do 
       defined?(Tag).should be_true
+    end
+    
+    it 'should be invalid without a name' do
+      @tag.should_not be_valid
+    end
+    
+    it 'should include an error message when the name is blank' do
+      @tag.errors[:name].should == ["can't be blank"]
     end
   end
 end
